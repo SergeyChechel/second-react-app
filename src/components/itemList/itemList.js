fc67483f55a00cdcf865c0945e5b01dc745e0c6e
Sergey <chechelsk@gmail.com>
2020-12-01 10:17:47 +0200

    Компоненты высшего порядка

--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -1,45 +1,30 @@
 import React, {Component} from 'react';
 import Spinner from '../spinner/';
 import './itemList.css';
-export default class ItemList extends Component {
+import PropTypes from 'prop-types'
+import GotService from '../../services/gotService';
 
-    state = {
-        itemList: null
-    };
-
-    componentDidMount() {
-        const {getData} = this.props;
-
-        getData()
-            .then(itemList => {
-                this.setState({itemList});
-            })
-    }
-
-    renderItems(arr) {
+const ItemList = (props) => {
+    
+    const renderItems = (arr) => {
         return arr.map((item, i) => {
             const {id} = item;
-            const label = this.props.renderItem(item);
+            const label = props.renderItem(item);
             return (
                 <li 
                     key={id}
                     className="list-group-item"
-                    onClick={() => this.props.onItemSelected(id)}
-                >
+                    onClick={() => props.onItemSelected(id)}>
                     {label}
                 </li>
             )
         });
     }
 
-    render() {
-        const {itemList} = this.state;
-
-        if (!itemList) {
-            return <Spinner/>
-        }
-
-        const items = this.renderItems(itemList);
+    function render() {
+        const {data} = props;
+        const items = renderItems(data);
+        console.log(items);
 
         return (
             <ul className="item-list list-group">
@@ -47,4 +32,48 @@ export default class ItemList extends Component {
             </ul>
         );
     }
+    
+    return render();
+
 }
+
+ItemList.defaultProps = {
+    onItemSelected: () => {},
+}
+
+ItemList.propTypes = {
+    onItemSelected: PropTypes.func
+}
+
+const withData = (View, getData) => {
+    return class extends Component {
+        
+        state = {
+            data: null
+        }
+    
+        componentDidMount() {
+            getData()
+                .then(data => {
+                    this.setState({data});
+                })
+        }
+
+        
+        render () {
+            console.log(this.state)
+            const {data} = this.state;
+            console.log(data);
+            if (!data) {
+                return <Spinner/>
+            }
+
+            return <View {...this.props} data={data}/>
+        }
+
+    };
+};
+
+const {getAllCharacters} = new GotService();
+
+export default withData(ItemList, getAllCharacters);
--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -3,6 +3,7 @@ import './randomChar.css';
 import GotService from '../../services/gotService';
 import Spinner from '../spinner/';
 import ErrorMessage from '../errorMessage/';
+import PropTypes from 'prop-types'
 
 export default class RandomChar extends Component {
 
@@ -15,7 +16,7 @@ export default class RandomChar extends Component {
 
     componentDidMount() {
         this.updateChar();
-        this.timerId = setInterval(this.updateChar, 1500);
+        this.timerId = setInterval(this.updateChar, this.props.interval);
     }
 
     componentWillUnmount() {
@@ -62,6 +63,13 @@ export default class RandomChar extends Component {
     }
 }
 
+RandomChar.defaultProps = {
+    interval: 15000
+}
+
+RandomChar.propTypes = {
+    interval: PropTypes.number
+}
 
 const View = ({char}) => {
     const {name, gender, born, died, culture} = char;
